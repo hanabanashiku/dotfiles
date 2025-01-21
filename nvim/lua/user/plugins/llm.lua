ae2c2683e5d6c5a0215fcac3dadb880b0b65e6e3
Michael MacLean <20232905+hanabanashiku@users.noreply.github.com>
2025-01-20 22:32:54 -0500

    Bump copilot chat version

--- nvim/lua/user/plugins/llm.lua
+++ nvim/lua/user/plugins/llm.lua
@@ -19,7 +19,7 @@ return {
 	"github/copilot.vim",
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
+		branch = "main",
 		dependencies = {
 			{ "github/copilot.vim" },
 			{ "nvim-lua/plenary.nvim" },
